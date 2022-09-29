4cea4794e7551bf8bd24b410bb36f552cbc22c90
Moritz Kirmse <moritz.kirmse@camptocamp.com>
2022-09-29 12:23:40 +0200

    use flexbox for layout

--- src/components/background-selector/background-selector.component.ts
+++ src/components/background-selector/background-selector.component.ts
@@ -31,18 +31,30 @@ export class BackgroundSelector extends LuxBase {
   
   render() {
     return html`
-<div class="w-10 md:w-auto float-right ${this.isOpened == true ? 'block' : 'hidden'}">
+<div class="flex flex-row-reverse">
+<div class="flex flex-col md:flex-row ${this.isOpened == true ? 'block' : 'hidden'}">
   ${this.bgLayers.map((layer, index) =>
-      html`<background-selector-item class="float-right p-0.5 rounded-sm ${layer == this.activeLayer ? 'border-red-500 border-2' : 'border-black border'} cursor-pointer hover:bg-cyan-600 md:h-14 md:w-20 h-10 w-10" bgclass="bg-${layer}" @click="${() => this.setBackgroundLayer(layer)}"></background-selector-item>`)}
+      html`<background-selector-item
+            class="lux-bg-sel ${''
+                   }${layer == this.activeLayer ? 'border-red-500 border-2' : 'border-black border'} ${''
+                   }hover:bg-cyan-600"
+            bgclass="bg-${layer}"
+            @click="${() => this.setBackgroundLayer(layer)}">
+      </background-selector-item>`)}
 </div>
-<div class="h-10 w-10 md:h-14 md:w-20 p-1 float-right p-0.5 rounded-sm border border-black cursor-pointer bg-white bg-selector-opener ${this.isOpened == true ? 'hidden' : 'block'}" @click="${this.toggleSelector}">
-  <img class="h-full w-full rounded-sm bg-[length:41px_41px] md:bg-[length:90px_50px] bg-${this.activeLayer}_sm md:bg-${this.activeLayer} hd:bg-${this.activeLayer}_sm_hi hd_md:bg-${this.activeLayer}_hi">
+<div class="lux-bg-sel border border-black bg-white bg-selector-opener ${''
+            }${this.isOpened == true ? 'hidden' : 'block'}"
+     @click="${this.toggleSelector}">
+  <img class="h-full w-full rounded-sm ${''
+              }bg-[length:41px_41px] md:bg-[length:90px_50px] ${''
+              }bg-${this.activeLayer}_sm md:bg-${this.activeLayer} ${''
+              }hd:bg-${this.activeLayer}_sm_hi hd_md:bg-${this.activeLayer}_hi">
+</div>
 </div>
 `;
   }
 
   setBackgroundLayer(layer) {
-    // this.activeLayer = layer;
     this.bgLayerService.activeBgLayer$.next(layer)
     this.isOpened = false;
     console.log(layer);
