2c6b84ee003c7108dee0dc974b306ef106d8e1be
Florent gravin <florent.gravin@camptocamp.com>
2022-10-05 09:53:24 +0200

    feat(map): add smart map container
    it host the map and initialize the state synchronization

--- src/components/map/map.container.ts
+++ src/components/map/map.container.ts
@@ -1,13 +1,17 @@
 import { html, LitElement, TemplateResult } from 'lit'
 import { customElement } from 'lit/decorators'
 import { mapService } from '../../services/map/map.service'
+import { OlSynchronizer } from '../../services/map/ol.synchronizer'
 
 @customElement('lux-map-container')
 export class MapContainer extends LitElement {
   connectedCallback() {
     super.connectedCallback()
     // todo: find the correct lifecycle method
-    setTimeout(() => mapService.createMap('map'))
+    setTimeout(() => {
+      mapService.createMap('map')
+      new OlSynchronizer(mapService.map)
+    })
   }
 
   render(): TemplateResult {
