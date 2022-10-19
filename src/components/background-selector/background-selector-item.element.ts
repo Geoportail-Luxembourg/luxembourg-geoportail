5fd866cef9bc9310032f4169e94f3713290d9b21
Moritz Kirmse <moritz.kirmse@camptocamp.com>
2022-10-19 17:44:50 +0200

    add tooltip translations

--- src/components/background-selector/background-selector-item.element.ts
+++ src/components/background-selector/background-selector-item.element.ts
@@ -1,5 +1,6 @@
 import { html, LitElement } from 'lit'
 import { customElement, property } from 'lit/decorators.js'
+import i18next from 'i18next'
 import { i18nMixin } from '../../mixins/i18n-lit-element'
 
 @customElement('lux-background-selector-item')
@@ -13,7 +14,7 @@ export class BackGroundSelectorItemElement extends i18nMixin(LitElement) {
 
   render() {
     // prettier-ignore
-    return html`<button title="${this.bgTitle}${this.bgTitle.length>0?'\n':''}Background layer: ${this.bgName}"
+    return html`<button title="${i18next.t(this.bgTitle)}${this.bgTitle.length>0?'\n':''}${i18next.t('Background layer')}: ${i18next.t(this.bgName)}"
       class="${`h-full w-full rounded-sm lux-bg-sel-icon-bg-length bg-white ` +
                `bg-${this.bgName}_sm md:bg-${this.bgName} hd:bg-${this.bgName}_sm_hi hd_md:bg-${this.bgName}_hi`}"
     />`
