dbedfa85954a8f9fbfcafc4339edcd06994087a6
AlitaBernachot <alita.bernachot@camptocamp.com>
2022-10-25 11:08:21 +0200

    chore(build): working build esm with styles

--- src/module.ts
+++ src/module.ts
@@ -1,1 +1,4 @@
-export * from './services/map/map.service'
+import './styles/tailwind.global.css'
+
+export { mapService, MapService } from './services/map/map.service'
+export { Dropdown } from './components/common/dropdown.component'
