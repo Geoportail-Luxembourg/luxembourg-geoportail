4573ed22199674aad86ce5352a366e3e9b64a463
Tobias Kohr <tobias.kohr@camptocamp.com>
2022-10-26 15:35:10 +0200

    feat(layer-metadata): display metadata over map

--- src/components/layers-panel/catalog/layer-metadata/layer-metadata.component.ts
+++ src/components/layers-panel/catalog/layer-metadata/layer-metadata.component.ts
@@ -11,36 +11,45 @@ export class LayerMetadata extends i18nMixin(LitElement) {
   render(): TemplateResult {
     const content = this.layerMetadata
       ? html`
-          <button @click="${this.closeMetadata}">X</button>
-          <h1>${i18next.t(`${this.layerMetadata.title}`)}</h1>
-          <dl>
-            <dt>${i18next.t('Name')}</dt>
-            <dd>${this.layerMetadata.name}</dd>
-            <dt>${i18next.t('Description du Service')}</dt>
-            <dd>${this.layerMetadata.serviceDescription}</dd>
-            <!--WMS/WMTS-->
-            <dt>${i18next.t('Description')}</dt>
-            <dd>${this.layerMetadata.description}</dd>
-            <!--short_trusted_description || trusted_description-->
-            <dt>${i18next.t("Contrainte d'utilisation")}</dt>
-            <dd>${this.layerMetadata.legalConstraints}</dd>
-            <dt>${i18next.t('Url vers la resource')}</dt>
-            <dd>${this.layerMetadata.link}</dd>
-            <dt>${i18next.t('Revision date')}</dt>
-            <dd>${this.layerMetadata.revisionDate}</dd>
-            <dt>${i18next.t('Keywords')}</dt>
-            <dd>${this.layerMetadata.keyword}</dd>
-            <dt>${i18next.t('Contact')}</dt>
-            <dd>${this.layerMetadata.responsibleParty}</dd>
-            <dt>${i18next.t('Link to the metadata')}</dt>
-            <dd>${this.layerMetadata.metadataLink}</dd>
-            <!--{{content.geonetworkBaseUrl}}/fre/catalog.search#/metadata/{{content.uid}}-->
-            <dt>${i18next.t('The metadata is right now not available')}</dt>
-            <dd>${this.layerMetadata.isError}</dd>
-            <dt>${i18next.t('Legend')}</dt>
-            <dd>${this.layerMetadata.legendHtml}</dd>
-            <!--hasLegend-->
-          </dl>
+          <div
+            class="fixed right-8 top-96 z-50 bg-white lux-modal w-[600px] p-3"
+            role="dialog"
+          >
+            <div class="lux-modal-header flex flex-row justify-between">
+              <h1>${i18next.t(`${this.layerMetadata.title}`)}</h1>
+              <button @click="${this.closeMetadata}">X</button>
+            </div>
+            <div class="pt-3">
+              <dl>
+                <dt>${i18next.t('Name')}</dt>
+                <dd>${this.layerMetadata.name}</dd>
+                <dt>${i18next.t('Description du Service')}</dt>
+                <dd>${this.layerMetadata.serviceDescription}</dd>
+                <!--WMS/WMTS-->
+                <dt>${i18next.t('Description')}</dt>
+                <dd>${this.layerMetadata.description}</dd>
+                <!--short_trusted_description || trusted_description-->
+                <dt>${i18next.t("Contrainte d'utilisation")}</dt>
+                <dd>${this.layerMetadata.legalConstraints}</dd>
+                <dt>${i18next.t('Url vers la resource')}</dt>
+                <dd>${this.layerMetadata.link}</dd>
+                <dt>${i18next.t('Revision date')}</dt>
+                <dd>${this.layerMetadata.revisionDate}</dd>
+                <dt>${i18next.t('Keywords')}</dt>
+                <dd>${this.layerMetadata.keyword}</dd>
+                <dt>${i18next.t('Contact')}</dt>
+                <dd>${this.layerMetadata.responsibleParty}</dd>
+                <dt>${i18next.t('Link to the metadata')}</dt>
+                <dd>${this.layerMetadata.metadataLink}</dd>
+                <!--{{content.geonetworkBaseUrl}}/fre/catalog.search#/metadata/{{content.uid}}-->
+                <dt>${i18next.t('The metadata is right now not available')}</dt>
+                <dd>${this.layerMetadata.isError}</dd>
+                <dt>${i18next.t('Legend')}</dt>
+                <dd>${this.layerMetadata.legendHtml}</dd>
+                <!--hasLegend-->
+              </dl>
+            </div>
+          </div>
         `
       : html``
     return content
