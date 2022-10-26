6a36884eddcaf7b5614dd1fd5b92469fc569b858
AlitaBernachot <alita.bernachot@camptocamp.com>
2022-10-26 17:54:51 +0200

    chore(parcel): use default parcel export module conf, and customize it

--- src/module.ts
+++ src/module.ts
@@ -2,3 +2,4 @@ import './styles/tailwind.global.css'
 
 export { mapService, MapService } from './services/map/map.service'
 export { Dropdown } from './components/common/dropdown.component'
+export { Catalog } from './components/layers-panel/catalog/Catalog'
