399c83906dba2e6aacb4709f6b6ca61b98f04346
Florent gravin <florent.gravin@camptocamp.com>
2022-11-09 15:53:17 +0100

    lib: don't import css in the lib bundle
    css are meant to be exported instead

--- src/module.ts
+++ src/module.ts
@@ -1,6 +1,3 @@
-import '@fortawesome/fontawesome-free/css/all.min.css'
-import './styles/tailwind.global.css'
-
 export { mapService, MapService } from './services/map/map.service'
 export { Dropdown } from './components/common/dropdown.component'
 export { Catalog } from './components/layers-panel/catalog/Catalog'
