29995c2ab8126b232990ea40a87b8ff6c933b453
AlitaBernachot <alita.bernachot@camptocamp.com>
2022-11-18 09:44:21 +0100

    feat(bg-layer): handle layer exclusion

--- src/services/background-layer/background-layer.service.ts
+++ src/services/background-layer/background-layer.service.ts
@@ -2,6 +2,7 @@ import { themesService } from '../themes/themes.service'
 import { mapState } from '../../state/map/map.state'
 import { Layer } from '../../state/map/map.state.model'
 import { bgConfig } from '../../../test/fixtures/background.config.fixture'
+import { layersServices } from '../layers/layers.service'
 
 class BackgroundLayerService {
   setBgLayer(layerId: number) {
@@ -17,6 +18,7 @@ class BackgroundLayerService {
         )
       }
       bgLayer.type = 'BG WMTS'
+      layersServices.excludeLayers(bgLayer)
       mapState.setBgLayer(bgLayer)
     } else {
       mapState.setBgLayer(null)
