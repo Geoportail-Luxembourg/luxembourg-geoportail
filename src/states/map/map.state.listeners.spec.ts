a599bac237d7129b3b3000ec83d9a5fe72207539
AlitaBernachot <alita.bernachot@camptocamp.com>
2022-11-18 16:46:15 +0100

    refactor(guidelines): .test.ts file extensions become .spec.ts

--- src/states/map/map.state.listeners.spec.ts
+++ src/states/map/map.state.listeners.spec.ts
@@ -1,6 +1,6 @@
 import { expect } from '@open-wc/testing'
-import { Layer, MapContext } from '../../src/states/map/map.state.model'
-import { MapSateListener } from '../../src/states/map/map.state.listeners'
+import { Layer, MapContext } from './map.state.model'
+import { MapSateListener } from './map.state.listeners'
 
 const SAMPLE_LAYERS: Layer[] = [
   {
