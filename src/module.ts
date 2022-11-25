36f0487906adf4bae021550a8e4f5b982da67376
AlitaBernachot <alita.bernachot@camptocamp.com>
2022-11-25 10:00:08 +0100

    feat(vuejs): test using module.exports for mounting inside the html

--- src/module.ts
+++ src/module.ts
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
 import TreeView from './vuejs/treeview.vue'
 
-export const lux = createApp(TreeView)
-// lux.component('Treeview', TreeView)
-lux.mount('#app')
+const treeviewApp = createApp(TreeView)
+// // lux.component('Treeview', TreeView)
+// lux.mount('#app')
 
-console.log(lux)
+console.log('Module esm')
+
+module.exports = { treeviewApp, TreeView }
