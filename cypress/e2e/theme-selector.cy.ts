20151c7f93e481f7c0a9f19f60f0b727750c74de
AlitaBernachot <alita.bernachot@camptocamp.com>
2023-02-15 18:37:24 +0100

    fix: adapt bg color for language selector according to theme

--- cypress/e2e/theme-selector.cy.ts
+++ cypress/e2e/theme-selector.cy.ts
@@ -9,16 +9,37 @@ describe('Theme selector', () => {
       .should('have.css', 'background-color', 'rgb(151, 187, 211)')
   })
 
-  it('updates the colors of the whole app while switching theme', () => {
-    cy.get('[data-cy="catalogButton"]').click()
-    cy.get('[data-cy="themeSelectorButton"]').click()
-    cy.get('[data-cy="themeSelectorButton"]')
-      .parent()
-      .find('Button')
-      .contains('Tourisme')
-      .click()
-    cy.get('[data-cy="layerPanel"]')
-      .parent()
-      .should('have.css', 'background-color', 'rgb(232, 140, 131)')
+  describe('When switching theme', () => {
+    beforeEach(() => {
+      cy.get('[data-cy="catalogButton"]').click()
+      cy.get('[data-cy="themeSelectorButton"]').click()
+      cy.get('[data-cy="themeSelectorButton"]')
+        .parent()
+        .find('Button')
+        .contains('Tourisme')
+        .click()
+    })
+
+    it('updates the colors of the whole app while switching theme', () => {
+      cy.get('[data-cy="layerPanel"]')
+        .parent()
+        .should('have.css', 'background-color', 'rgb(232, 140, 131)')
+    })
+
+    it('updates the colors of the language selector', () => {
+      cy.get('[data-cy="langSelect"] .lux-dropdown-btn')
+        .click()
+        .should('have.css', 'background-color', 'rgb(232, 76, 61)')
+
+      cy.get('[data-cy="langSelect"] .lux-dropdown-list li:not(.selected)')
+        .should('have.length', 3)
+        .each($el => {
+          cy.wrap($el).should(
+            'have.css',
+            'background-color',
+            'rgb(232, 76, 61)'
+          )
+        })
+    })
   })
 })
