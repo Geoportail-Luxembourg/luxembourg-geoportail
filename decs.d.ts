4a8b1752603ca860460800b1460fd165115b69d5
Moritz Kirmse <moritz.kirmse@camptocamp.com>
2023-02-20 10:13:54 +0100

    fix type declaration

--- decs.d.ts
+++ decs.d.ts
@@ -1,7 +1,2 @@
-import type BaseLayer from 'ol/layer/Base'
 declare module '@camptocamp/ogc-client'
-declare module '@geoblocks/ol-maplibre-layer' {
-  export default class MapLibreLayer extends BaseLayer {
-    constructor() {}
-  }
-}
+declare module '@geoblocks/ol-maplibre-layer'
