535640997ceba1fe13208ba2821f87e2fe2b2c88
Moritz Kirmse <moritz.kirmse@camptocamp.com>
2023-02-27 10:15:46 +0100

    improve reactive computation of style status => highlight simple styles

--- src/components/style-selector/simple-style-selector.vue
+++ src/components/style-selector/simple-style-selector.vue
@@ -1,15 +1,20 @@
 <script setup lang="ts">
 import { useTranslation } from 'i18next-vue'
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 
 import { useStyleStore } from '@/stores/style.store'
 // import { statePersistorStyleService } from '@/services/state-persistor/state-persistor-style.service'
 import SimpleStyleItem from './simple-style-item.vue'
 import { bgConfigFixture } from '@/__fixtures__/background.config.fixture'
 import { SimpleRoadStyle } from '@/composables/mvt-styles/mvt-styles.model'
+import { storeToRefs } from 'pinia'
+import useMvtStyles from '@/composables/mvt-styles/mvt-styles.composable'
 
 const { t } = useTranslation()
 const styleStore = useStyleStore()
+const styleService = useMvtStyles()
+const { bgStyle } = storeToRefs(styleStore)
+const simpleStyles = ref(bgConfigFixture().simple_styles)
 
 // statePersistorStyleService.bootstrapStyle()
 
@@ -19,21 +24,17 @@ const props = defineProps({
     default: true,
   },
 })
-const simpleStyles = ref(bgConfigFixture().simple_styles)
+const simpleStyleConf = ref(bgConfigFixture().simple_styles)
+// does not seem to work with "computed" => therefore using "watch"
+watch(
+  [bgStyle, simpleStyleConf],
+  ([st, ss]) => (simpleStyles.value = styleService.checkSelection(st || [], ss))
+)
 
 function onStylingSelected(item: SimpleRoadStyle) {
-  simpleStyles.value = simpleStyles.value.map(st => {
-    if (st.unlocalized_label == item.unlocalized_label) {
-      st.selected = true
-    } else {
-      st.selected = false
-    }
-    return st
-  })
   styleStore.setSimpleStyle(item)
 }
 function resetStyle() {
-  simpleStyles.value = bgConfigFixture().simple_styles
   styleStore.setSimpleStyle(null)
 }
 </script>
