dc1241e6c5c0540f6a03d3d5c564e51bab8b6686
AlitaBernachot <alita.bernachot@camptocamp.com>
2023-04-04 15:57:56 +0200

    add catalog

--- src/lib.ts
+++ src/lib.ts
@@ -1,4 +1,5 @@
 import './assets/main.css'
 import DropdownList from '@/components/common/dropdown-list.vue'
+import Catalog from '@/components/catalog/catalog-tree.vue'
 
-export { DropdownList }
+export { DropdownList, Catalog }
