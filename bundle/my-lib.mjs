4319621196a473938c7c7b793682f380ec6ff2b2
AlitaBernachot <alita.bernachot@camptocamp.com>
2023-04-04 14:44:33 +0200

    add css to lib

--- bundle/my-lib.mjs
+++ bundle/my-lib.mjs
@@ -1,5 +1,5 @@
-import { defineComponent as f, shallowRef as v, onMounted as k, onUnmounted as w, openBlock as o, createElementBlock as s, createElementVNode as l, normalizeClass as d, unref as i, toDisplayString as _, Fragment as g, renderList as C } from "vue";
-const L = { class: "lux-dropdown" }, B = { class: "h-full" }, E = ["aria-expanded"], V = /* @__PURE__ */ l("span", { class: "lux-caret" }, null, -1), y = { class: "lux-dropdown-wrapper" }, D = ["aria-label", "data-value"], z = /* @__PURE__ */ f({
+import { defineComponent as f, shallowRef as v, onMounted as k, onUnmounted as w, openBlock as o, createElementBlock as s, createElementVNode as n, normalizeClass as d, unref as i, toDisplayString as _, Fragment as g, renderList as C } from "vue";
+const L = { class: "lux-dropdown" }, B = { class: "h-full" }, E = ["aria-expanded"], V = /* @__PURE__ */ n("span", { class: "lux-caret" }, null, -1), y = { class: "lux-dropdown-wrapper" }, D = ["aria-label", "data-value"], z = /* @__PURE__ */ f({
   __name: "dropdown-list",
   props: {
     placeholder: null,
@@ -7,16 +7,16 @@ const L = { class: "lux-dropdown" }, B = { class: "h-full" }, E = ["aria-expande
     modelValue: null
   },
   emits: ["change"],
-  setup(u, { emit: h }) {
-    const a = u, n = v(!1), c = v();
+  setup(c, { emit: h }) {
+    const a = c, l = v(!1), u = v();
     function r(e) {
-      n.value = e === void 0 ? !n.value : e;
+      l.value = e === void 0 ? !l.value : e;
     }
     function x(e) {
       e.stopImmediatePropagation(), r();
     }
     function b(e) {
-      c.value = e.target.dataset.value, h("change", c.value);
+      u.value = e.target.dataset.value, h("change", u.value);
     }
     function p() {
       r(!1);
@@ -24,28 +24,28 @@ const L = { class: "lux-dropdown" }, B = { class: "h-full" }, E = ["aria-expande
     return k(() => document.addEventListener("click", p)), w(() => document.removeEventListener("click", p)), (e, O) => {
       var m;
       return o(), s("div", L, [
-        l("div", B, [
-          l("button", {
+        n("div", B, [
+          n("button", {
             type: "button",
-            class: d(["lux-btn lux-dropdown-btn", i(n) ? "expanded" : ""]),
-            "aria-expanded": i(n),
+            class: d(["lux-btn lux-dropdown-btn", i(l) ? "expanded" : ""]),
+            "aria-expanded": i(l),
             "aria-haspopup": "true",
             onClick: x
           }, [
-            l("span", null, _((m = a.placeholder) != null ? m : a.options[0].label), 1),
+            n("span", null, _((m = a.placeholder) != null ? m : a.options[0].label), 1),
             V
           ], 10, E)
         ]),
-        l("div", y, [
-          l("ul", {
-            class: d(["lux-dropdown-list", i(n) ? "" : "hidden"]),
+        n("div", y, [
+          n("ul", {
+            class: d(["lux-dropdown-list", i(l) ? "" : "hidden"]),
             tabindex: "-1"
           }, [
             (o(!0), s(g, null, C(a.options, (t) => (o(), s("li", {
               key: t.value,
-              class: d(u.modelValue === t.value ? "selected" : "")
+              class: d(c.modelValue === t.value ? "selected" : "")
             }, [
-              l("button", {
+              n("button", {
                 class: "lux-dropdown-list-item",
                 "aria-label": t.ariaLabel,
                 "data-value": t.value,
--- src/lib.ts
+++ src/lib.ts
@@ -1,3 +1,4 @@
+import './assets/main.css'
 import DropdownList from '@/components/common/dropdown-list.vue'
 
 export { DropdownList }
