fb8393beee03cd285afaa435987a6a1cd63f6e86
AlitaBernachot <alita.bernachot@camptocamp.com>
2023-04-05 15:09:55 +0200

    test with new wrapper for web comp (OK)

--- src/components/catalog/catalog-tree.vue
+++ src/components/catalog/catalog-tree.vue
@@ -43,6 +43,8 @@ function toggleParent(node: LayerTreeNodeModel) {
 function toggleLayer(node: LayerTreeNodeModel) {
   layers.toggleLayer(+node.id, !node.checked)
 }
+
+console.log("catalog-tree.vue")
 </script>
 
 <template>
