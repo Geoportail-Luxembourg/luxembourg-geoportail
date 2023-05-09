0d94d74ec74de4eea1ba833f8cf582833816c011
Tobias Kohr <tobias.kohr@camptocamp.com>
2023-05-09 16:42:53 +0200

    feat(themes): allow setting themes from outside app/lib
    use fixture for app, allow to pass real data to lib calling setThemes

--- src/stores/config.store.ts
+++ src/stores/config.store.ts
@@ -1,13 +1,11 @@
 import { acceptHMRUpdate, defineStore } from 'pinia'
 import { computed, ref, shallowRef, ShallowRef } from 'vue'
 
-import useThemes from '@/composables/themes/themes.composable'
 import { ConfigModel } from '@/composables/themes/themes.model'
 
 export const useThemeStore = defineStore(
   'config',
   () => {
-    const themesService = useThemes()
     const config: ShallowRef<ConfigModel | undefined> = shallowRef()
     const themeName = ref('main')
     const themes = computed(() => config.value?.themes)
@@ -16,13 +14,23 @@ export const useThemeStore = defineStore(
     )
     const bgLayers = computed(() => config.value?.background_layers || [])
 
-    themesService.fetchThemes().then(response => (config.value = response))
+    function setThemes(themes: ConfigModel) {
+      config.value = themes
+    }
 
     function setTheme(name: string) {
       themeName.value = name
     }
 
-    return { config, themes, themeName, theme, bgLayers, setTheme }
+    return {
+      config,
+      themes,
+      themeName,
+      theme,
+      bgLayers,
+      setTheme,
+      setThemes,
+    }
   },
   {}
 )
