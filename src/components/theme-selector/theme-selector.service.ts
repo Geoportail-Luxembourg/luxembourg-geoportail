4aee7a973613a34b7c35ee66969ae46a709517ce
Tobias Kohr <tobias.kohr@camptocamp.com>
2023-05-10 15:12:25 +0200

    refactor(theming): pass only theme name to setCurrentThemeColors

--- src/components/theme-selector/theme-selector.service.ts
+++ src/components/theme-selector/theme-selector.service.ts
@@ -1,12 +1,10 @@
-import { ThemeNodeModel } from '@/composables/themes/themes.model'
-
 export class ThemeSelectorService {
-  setCurrentThemeColors(theme: ThemeNodeModel) {
+  setCurrentThemeColors(themeName: string) {
     const root = document.querySelector(':root') as HTMLElement
     const colors = ['primary', 'secondary', 'tertiary']
     colors.forEach(colorTone => {
       const color = getComputedStyle(root).getPropertyValue(
-        `--${theme.name}-${colorTone}`
+        `--${themeName}-${colorTone}`
       )
       root.style.setProperty(`--color-${colorTone}`, color)
     })
