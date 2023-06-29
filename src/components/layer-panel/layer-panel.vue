332d196e7614cfa8d85cb82c837cf4f9c45a01a7
AlitaBernachot <alita.bernachot@camptocamp.com>
2023-06-29 16:31:18 +0200

    fix(layer-panel): hide counter in tab label when layer list is empty

--- src/components/layer-panel/layer-panel.vue
+++ src/components/layer-panel/layer-panel.vue
@@ -1,21 +1,22 @@
 <script setup lang="ts">
-import { ref } from 'vue'
 import { useTranslation } from 'i18next-vue'
+import { storeToRefs } from 'pinia'
+import { ref } from 'vue'
 
 import CatalogTab from '@/components/catalog/catalog-tab.vue'
 import LayerManager from '@/components/layer-manager/layer-manager.vue'
 import { useAppStore } from '@/stores/app.store'
 import { useMapStore } from '@/stores/map.store'
-import { storeToRefs } from 'pinia'
 
 const { t } = useTranslation()
-const myLayersOpen = ref(false)
 const { setLayersOpen } = useAppStore()
 const { layers } = storeToRefs(useMapStore())
+const myLayersOpen = ref(false)
 </script>
 
 <template>
   <div data-cy="layerPanel" class="flex flex-col h-full pt-1.5">
+    <!-- Panel title and close button -->
     <div class="h-16 shrink-0 flex justify-between lux-panel-title">
       <div>
         {{ t('Layers', { ns: 'client' }) }}
@@ -28,7 +29,8 @@ const { layers } = storeToRefs(useMapStore())
         ></button
       ></span>
     </div>
-    <!--selector tab-->
+
+    <!-- My Layers and Catalog tab labels -->
     <div class="flex flex-row gap-2 h-10 text-2xl">
       <button
         data-cy="myLayersButton"
@@ -37,7 +39,8 @@ const { layers } = storeToRefs(useMapStore())
         :class="myLayersOpen ? 'bg-primary' : 'bg-tertiary'"
         :aria-expanded="myLayersOpen"
       >
-        {{ t('my_layers', { ns: 'client' }) }} ({{ layers.length }})
+        {{ t('my_layers', { ns: 'client' }) }}
+        <span v-if="layers.length">({{ layers.length }})</span>
       </button>
       <button
         data-cy="catalogButton"
@@ -49,7 +52,8 @@ const { layers } = storeToRefs(useMapStore())
         {{ t('Catalog', { ns: 'client' }) }}
       </button>
     </div>
-    <!--catalog tab-->
+
+    <!-- Panel content (MyLayers and Catalog) -->
     <div class="relative grow p-2.5 bg-primary overflow-auto">
       <catalog-tab v-if="!myLayersOpen"></catalog-tab>
       <layer-manager
