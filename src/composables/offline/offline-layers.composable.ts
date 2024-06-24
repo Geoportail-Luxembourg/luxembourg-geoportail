8bcbba59cc153842d0e50d7499e72eef62cd6697
AlitaBernachot <alita.bernachot@camptocamp.com>
2024-06-24 09:37:36 +0200

    feat(offline): handle offline raster background

--- src/composables/offline/offline-layers.composable.ts
+++ src/composables/offline/offline-layers.composable.ts
@@ -1,27 +1,33 @@
 import { useMapStore } from '@/stores/map.store'
 import { Layer } from '@/stores/map.store.model'
 import useLayers from '@/composables/layers/layers.composable'
+import { LayerTypeValue } from '@/composables/themes/themes.model'
 
 import { OfflineLayerSpec, OfflineLayerTypeValue } from './offline.model'
 
 export default function useOfflineLayers() {
   const { initLayer } = useLayers()
-  const { addLayers } = useMapStore()
+  const { addLayers, setBgLayer } = useMapStore()
 
   function offlineLayerToLayer(offlineLayerSpec: OfflineLayerSpec): Layer {
     const options = JSON.parse(offlineLayerSpec.layerSerialization)
     const { id, label: name } = options
+    let type = <string>offlineLayerSpec.layerType
+
+    if (type === LayerTypeValue.BG_WMTS) {
+      type = OfflineLayerTypeValue.LAYER_OFFLINE_BG_WMTS
+    }
 
     return {
       id,
       name,
       options,
-      type: offlineLayerSpec.layerType,
+      type,
     } as unknown as Layer
   }
 
   /**
-   * Recreate the offline layer
+   * Recreate the offline layer and add to the store (will be auto added by the ol synchronizer)
    * @param offlineLayerSpec The layer spec to recreate the layer
    * @see recreateOfflineLayer in v3
    */
@@ -29,10 +35,13 @@ export default function useOfflineLayers() {
     if (
       offlineLayerSpec.layerType === OfflineLayerTypeValue.LAYER_OFFLINE_TILE
     ) {
-      // v3 cond. don't know why?
       const layer = initLayer(offlineLayerToLayer(offlineLayerSpec))
 
-      addLayers(layer)
+      if (offlineLayerSpec.backgroundLayer) {
+        setBgLayer(layer)
+      } else {
+        addLayers(layer)
+      }
     }
   }
 
