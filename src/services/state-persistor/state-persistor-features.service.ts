96e1005e347233ced1c967eccc1622517e35a42b
Tobias Kohr <tobias.kohr@camptocamp.com>
2024-08-07 15:25:37 +0200

    refactor(draw): prepare feature encoding in persistor

--- src/services/state-persistor/state-persistor-features.service.ts
+++ src/services/state-persistor/state-persistor-features.service.ts
@@ -1,9 +1,11 @@
-import { watch, watchEffect, WatchStopHandle } from 'vue'
+import { Ref, watch, watchEffect, WatchStopHandle } from 'vue'
 import { storeToRefs } from 'pinia'
 import { SP_KEY_FEATURES, StatePersistorService } from './state-persistor.model'
 import { storageHelper } from './storage/storage.helper'
 import { storageFeaturesMapper } from './state-persistor-features.mapper'
 import { useDrawStore } from '@/stores/draw.store'
+import { Collection, Feature } from 'ol'
+import { Geometry } from 'ol/geom'
 
 class StatePersistorFeaturesService implements StatePersistorService {
   bootstrap() {
@@ -20,16 +22,14 @@ class StatePersistorFeaturesService implements StatePersistorService {
     const { drawnFeatures } = storeToRefs(useDrawStore())
 
     watch(
-      drawnFeatures,
-      (value, oldValue) => {
+      drawnFeatures as Ref<Collection<Feature<Geometry>>>,
+      value => {
         console.log('persist', value)
-        if (oldValue !== value) {
-          storageHelper.setValue(
-            SP_KEY_FEATURES,
-            value,
-            storageFeaturesMapper.featuresToUrl
-          )
-        }
+        storageHelper.setValue(
+          SP_KEY_FEATURES,
+          value,
+          storageFeaturesMapper.featuresToUrl
+        )
       },
       { immediate: true, deep: true }
     )
