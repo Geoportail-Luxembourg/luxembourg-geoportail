17e74a06c0d01e586c4ea4f95399668bb2d5a698
Tobias Kohr <tobias.kohr@camptocamp.com>
2024-08-09 14:59:33 +0200

    refactor(draw): move statePersistorFeaturesService.bootstrap() to map-container
    this way no explicit  bootstrap will be needed in v3 via lib

--- src/bundle/lib.ts
+++ src/bundle/lib.ts
@@ -39,7 +39,6 @@ import { statePersistorThemeService } from '@/services/state-persistor/state-per
 import { statePersistorAppService } from '@/services/state-persistor/state-persistor-app.service'
 import { statePersistorStyleService } from '@/services/state-persistor/state-persistor-bgstyle.service'
 import { statePersistorMyMapService } from '@/services/state-persistor/state-persistor-mymap.service'
-import { statePersistorFeaturesService } from '@/services/state-persistor/state-persistor-features.service'
 import { proxyUrlHelper } from '@/services/proxyurl/proxyurl.helper'
 import { styleUrlHelper } from '@/services/styleurl/styleurl.helper'
 import { themeSelectorService } from '@/components/theme-selector/theme-selector.service'
@@ -145,7 +144,6 @@ export {
   statePersistorAppService,
   statePersistorStyleService,
   statePersistorMyMapService,
-  statePersistorFeaturesService,
   themeSelectorService,
   MapLibreLayer,
   StyleSelector,
