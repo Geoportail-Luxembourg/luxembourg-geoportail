a2f0fdd074c4597c8af70fa4d9f8fdb42de94f7e
AlitaBernachot <alita.bernachot@camptocamp.com>
2024-08-09 16:45:00 +0200

    feat: new dropdown menu

---
+++ src/components/common/menu-popup/menu-popup-item.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const props = defineProps<{
+  item: {label: string, action?: (event?: MouseEvent) => any}
+}>()
+const emit = defineEmits(['click'])
+
+function onClick() {
+  emit('click', props.item)
+}
+</script>
+
+<template>
+  <button
+      class="lux-dropdown-list-item"
+      @click="onClick"
+    >
+    <slot>{{ item.label }}</slot>
+    </button>
+</template>
