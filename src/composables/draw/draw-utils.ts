42bbb8368e41fa4d25df270d44b12fbdd7908cf8
Tobias Kohr <tobias.kohr@camptocamp.com>
2024-10-11 14:57:17 +0200

    feat(draw): enable editing circles on map
    keep circles as circle geometries in ol and convert them only to polygons for URL encoding

--- src/composables/draw/draw-utils.ts
+++ src/composables/draw/draw-utils.ts
@@ -1,6 +1,8 @@
-import { Circle, Geometry } from 'ol/geom'
-import { fromCircle } from 'ol/geom/Polygon'
-import { Feature } from 'ol'
+import { Circle } from 'ol/geom'
+import Polygon, { fromCircle } from 'ol/geom/Polygon'
+import { getDistance } from 'ol/sphere'
+import { toLonLat } from 'ol/proj'
+import { DrawnFeature } from '@/services/draw/drawn-feature'
 
 // TODO 3D
 // import { transform } from 'ol/proj'
@@ -9,14 +11,34 @@ import { Feature } from 'ol'
 // TODO 3D
 // const ARROW_MODEL_URL = import.meta.env.VITE_ARROW_MODEL_URL
 
-function convertCircleToPolygon(
-  feature: Feature<Geometry>,
-  featureType: String
-) {
+function convertCircleFeatureToPolygon(feature: DrawnFeature): DrawnFeature {
   const geom = feature.getGeometry()
-  if (featureType == 'drawnCircle' && geom?.getType() == 'Circle') {
+  if (feature.featureType == 'drawnCircle' && geom?.getType() == 'Circle') {
     feature.setGeometry(fromCircle(geom as Circle, 64))
   }
+  return feature
 }
 
-export { convertCircleToPolygon }
+function convertPolygonFeatureToCircle(feature: DrawnFeature): DrawnFeature {
+  const polygon = feature.getGeometry() as Polygon
+  if (
+    feature.featureType === 'drawnCircle' &&
+    polygon?.getType() === 'Polygon'
+  ) {
+    const extent = polygon.getExtent()
+    const centroid = [(extent[0] + extent[2]) / 2, (extent[1] + extent[3]) / 2]
+    const coordinates = polygon.getCoordinates()[0]
+    let maxDistance = 0
+    coordinates.forEach(coord => {
+      const distance = getDistance(toLonLat(centroid), toLonLat(coord))
+      if (distance > maxDistance) {
+        maxDistance = distance
+      }
+    })
+    const circle = new Circle(centroid, maxDistance)
+    feature.setGeometry(circle as Circle)
+  }
+  return feature
+}
+
+export { convertCircleFeatureToPolygon, convertPolygonFeatureToCircle }
