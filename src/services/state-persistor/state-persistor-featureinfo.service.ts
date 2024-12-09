60fb12bab504d1e18f0c76d89b6f7fd6a3d40dea
Tobias Kohr <tobias.kohr@camptocamp.com>
2024-12-09 18:24:20 +0100

    review(state-persistor): move removeQueryParam to urlStorage

--- src/services/state-persistor/state-persistor-featureinfo.service.ts
+++ src/services/state-persistor/state-persistor-featureinfo.service.ts
@@ -1,6 +1,7 @@
 import { SP_KEY_FID } from './state-persistor.model'
 import { storageHelper } from './storage/storage.helper'
 import { useFeatureInfoStore } from '@/stores/feature-info.store'
+import { urlStorage } from './storage/url-storage'
 
 class StatePersistorFeatureInfoService {
   bootstrap() {
@@ -13,14 +14,8 @@ class StatePersistorFeatureInfoService {
     if (fid) {
       const { setFid } = useFeatureInfoStore()
       setFid(fid)
-      this.removeQueryParam(SP_KEY_FID)
+      urlStorage.removeQueryParam(SP_KEY_FID)
     }
-  }
-
-  removeQueryParam(param: string): void {
-    const url = new URL(window.location.href)
-    url.searchParams.delete(param)
-    window.history.replaceState({}, document.title, url.toString())
   }
 }
 
