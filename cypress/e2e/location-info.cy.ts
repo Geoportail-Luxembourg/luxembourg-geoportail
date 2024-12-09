2dbe4174e4da5049d8f378282e79bbfd3b15b496
Moritz Kirmse <moritz.kirmse@camptocamp.com>
2024-12-09 18:22:41 +0100

    use fixed API responses to have predictable test results

--- cypress/e2e/location-info.cy.ts
+++ cypress/e2e/location-info.cy.ts
@@ -1,9 +1,47 @@
-describe('Feature Info', () => {
+describe('Location Info', () => {
   beforeEach(() => {
-    cy.intercept('GET', 'getfeatureinfo*').as('getFeatureInfo')
+    cy.intercept('POST', 'short/create', {
+      statusCode: 200,
+      body: { short_url: 'http://localhost:8080/s/uSxF' },
+    }).as('shortUrl')
+
+    cy.intercept(
+      'GET',
+      'https://migration.geoportail.lu/geocode/reverse?easting=67886.66280635976&northing=85024.54757489248',
+      {
+        statusCode: 200,
+        body: {
+          count: 1,
+          results: [
+            {
+              id_caclr_locality: '37',
+              id_caclr_street: '1147',
+              id_caclr_bat: '213956',
+              street: 'Bergstr',
+              number: '18',
+              locality: 'Roodt/Eisch/Test',
+              commune: 'Habscht',
+              postal_code: '8398',
+              country: 'Luxembourg',
+              country_code: 'lu',
+              distance: 1967.871691858786,
+              contributor: 'ACT',
+              geom: {
+                type: 'Point',
+                coordinates: [67900.5221, 84743.219699999],
+              },
+              geomlonlat: {
+                type: 'Point',
+                coordinates: [6.00041535, 49.697110053],
+              },
+            },
+          ],
+        },
+      }
+    )
   })
 
-  describe('Default template', () => {
+  describe('Open location info on position', () => {
     describe('Display basic feature info for multiple layers', () => {
       beforeEach(() => {
         cy.visit('/?zoom=8')
@@ -27,6 +65,8 @@ describe('Feature Info', () => {
         cy.get('div.ol-viewport').rightclick(350, 300, { force: true })
         cy.get('[data-cy="locationInfo"]').should('be.visible')
         cy.get('[data-cy="locationInfo"]').find('input').should('exist')
+        //cy.log(JSON.stringify(cy.get('[data-cy="locationInfo"]')
+        //  .find('input').invoke('val')))
         cy.get('[data-cy="locationInfo"]')
           .find('input')
           .invoke('val')
@@ -36,6 +76,14 @@ describe('Feature Info', () => {
           'have.length',
           8
         )
+        cy.get('[data-cy="locationInfo"] > div > table > tbody > tr')
+          .eq(6)
+          .find('td')
+          .should('contain.text', 'Roodt/Eisch/Test')
+        cy.get('[data-cy="locationInfo"] > div > table > tbody > tr')
+          .eq(7)
+          .find('td')
+          .should('contain.text', '1.97 km')
         // check pointer
         cy.window()
           .its('olMap')
@@ -81,7 +129,7 @@ describe('Feature Info', () => {
         cy.get('[data-cy="streetviewNoData"]').should('not.be.visible')
         // TODO: think about more reliable sync with google loanding times
         // eslint-disable-next-line
-        cy.wait(1000)
+        cy.wait(2000)
         cy.window()
           .its('olMap')
           .then(function (olMap) {
