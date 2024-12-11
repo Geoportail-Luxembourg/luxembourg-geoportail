00b7a3fbd27771b629e8a04ada65e6f7a2a29c23
AlitaBernachot <alita.bernachot@camptocamp.com>
2024-12-11 16:52:03 +0100

    test: add e2e tests for private symbols

--- cypress/e2e/legends/legends.cy.ts
+++ cypress/e2e/legends/legends.cy.ts
@@ -101,8 +101,7 @@ describe('Legends', () => {
   describe('When a layer with a legend is removed', () => {
     beforeEach(() => {
       cy.get('[data-cy="catalogButton"]').click()
-      cy.get('[data-cy^="layerLabel-262"]').click()
-
+      cy.get('[data-cy="layerLabel-262"]').click()
       cy.get('[data-cy="legendsOpenClose"] > button').click()
     })
 
