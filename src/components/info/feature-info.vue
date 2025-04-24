2c0662c163af8752eb6ca31ccaff486f9ee9a882
Michaëlis <renaud.michaelis@qct.etat.lu>
2025-04-24 17:42:44 +0200

    default_table_no_prefix

--- src/components/info/feature-info.vue
+++ src/components/info/feature-info.vue
@@ -24,6 +24,7 @@ import BusWoTitleTemplate from './templates/bus-wo-title-template.vue'
 import BusTemplate from './templates/bus-template.vue'
 import DefaultTemplate from './templates/default-template.vue'
 import DefaultTableTemplate from './templates/default-table-template.vue'
+import DefaultTableNoPrefixTemplate from './templates/default-table-no-prefix-template.vue'
 import LignesBusTemplate from './templates/lignes-bus-template.vue'
 import PcnPagTemplate from './templates/pcn-pag-template.vue'
 // import PdsTemplate from './templates/pds-template.vue'
@@ -68,6 +69,7 @@ const templates = {
   'default_attachment.html': DefaultAttachment,
   'default_attachment_no_prefix.html': DefaultAttachmentNoPrefixTemplate,
   'default_table.html': DefaultTableTemplate,
+  'default_table_no_prefix.html': DefaultTableNoPrefixTemplate,
   'lignes_bus.html': LignesBusTemplate,
   'parcels.html': ParcelsTemplate,
   'pcn_pag.html': PcnPagTemplate,
