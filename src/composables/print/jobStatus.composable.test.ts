77275c1e8110c4156edb9b02402fbe296716ac3d
AlitaBernachot <alita.bernachot@camptocamp.com>
2025-04-25 12:16:26 +0200

    fix: ut

--- src/composables/print/jobStatus.composable.test.ts
+++ src/composables/print/jobStatus.composable.test.ts
@@ -1,5 +1,8 @@
 import { printService } from '@/services/print/print.service'
-import { JobStatus } from '@/services/print/print.model'
+import {
+  JobStatus,
+  MapFishPrintStatusResponse,
+} from '@/services/print/print.model'
 import {
   afterEach,
   beforeEach,
@@ -17,7 +20,6 @@ vi.useFakeTimers()
 
 const getMockedFunction = () =>
   printService.getJobStatus as MockedFunction<typeof printService.getJobStatus>
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const mockGetJobStatus = (mockStatus: JobStatus) =>
   getMockedFunction().mockResolvedValue(mockStatus)
 
@@ -41,45 +43,46 @@ describe('useJobStatus', () => {
     expect(jobStatus.value).toBe(null)
   })
 
-  // it('should start polling and update jobStatus', async () => {
-  //   const { jobStatus, startPolling } = useJobStatus()
-  //   const mockStatus = { status: 'running', done: false }
-  //   mockGetJobStatus(mockStatus)
-  //   console.log('mock', await printService.getJobStatus('test-job-id'))
+  it('should start polling and update jobStatus', async () => {
+    const { jobStatus, startPolling } = useJobStatus()
+    const mockStatus = {
+      status: <MapFishPrintStatusResponse>'running',
+      done: false,
+    }
 
-  //   startPolling(jobId)
-  //   await vi.advanceTimersByTime(1000)
-  //   await nextTick()
+    mockGetJobStatus(mockStatus)
+    startPolling(jobId)
+    vi.advanceTimersByTime(1000)
+    await nextTick()
 
-  //   expect(printService.getJobStatus).toHaveBeenCalledWith(jobId)
-  //   expect(jobStatus.value).toBe('running')
-  // })
+    expect(printService.getJobStatus).toHaveBeenCalledWith(jobId)
+    expect(jobStatus.value).toBe('running')
+  })
 
-  // it('should open download URL and clear polling when job is finished', async () => {
-  //   const { jobStatus, startPolling, url } = useJobStatus()
-  //   const mockStatus = {
-  //     status: 'finished',
-  //     done: true,
-  //     downloadURL: 'http://example.com',
-  //   }
-  //   mockGetJobStatus(mockStatus)
-  //   console.log('mock', await printService.getJobStatus('test-job-id'))
+  it('should open download URL and clear polling when job is finished', async () => {
+    const { jobStatus, startPolling, url } = useJobStatus()
+    const mockStatus = {
+      status: <MapFishPrintStatusResponse>'finished',
+      done: true,
+      downloadURL: 'http://example.com',
+    }
 
-  //   startPolling(jobId)
-  //   await vi.advanceTimersByTime(1000)
-  //   await nextTick()
+    mockGetJobStatus(mockStatus)
+    startPolling(jobId)
+    vi.advanceTimersByTime(1000)
+    await nextTick()
 
-  //   expect(printService.getJobStatus).toHaveBeenCalledWith(jobId)
-  //   expect(url.value).toBe('http://example.com')
-  //   expect(jobStatus.value).toBe('finished')
-  // })
+    expect(printService.getJobStatus).toHaveBeenCalledWith(jobId)
+    expect(url.value).toBe('http://example.com')
+    expect(jobStatus.value).toBe(null) // null because we clear polling
+  })
 
   it('should clear polling on error', async () => {
     const { jobStatus, startPolling } = useJobStatus()
     getMockedFunction().mockRejectedValue(new Error('Test error'))
 
     startPolling(jobId)
-    await vi.advanceTimersByTime(1000)
+    vi.advanceTimersByTime(1000)
     await nextTick()
 
     expect(printService.getJobStatus).toHaveBeenCalledWith(jobId)
