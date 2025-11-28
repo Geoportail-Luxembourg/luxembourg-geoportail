7573a74bb9968641bbe6d6939f5e28288faaa8e7
Michaëlis <renaud.michaelis@qct.etat.lu>
2025-11-28 13:57:53 +0100

    No more misleading alias or double negative

--- src/components/common/network-indicator.vue
+++ src/components/common/network-indicator.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    v-if="!isOnline"
+    v-if="isOffLine"
     class="fixed top-0 left-0 right-0 bg-yellow-500 text-white text-center py-2 z-50"
     role="alert"
     aria-live="assertive"
@@ -14,5 +14,5 @@
 import { storeToRefs } from 'pinia'
 import { useAppStore } from '@/stores/app.store'
 
-const { isOffLine: isOnline } = storeToRefs(useAppStore())
+const { isOffLine } = storeToRefs(useAppStore())
 </script>
