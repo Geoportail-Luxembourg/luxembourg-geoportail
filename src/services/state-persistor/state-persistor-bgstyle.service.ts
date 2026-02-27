44e4c0a752f091fe3ad5685c37aadc243ce9ecdd
Michaëlis <renaud.michaelis@act.etat.lu>
2026-02-27 17:19:27 +0100

    remove serial from url

--- src/services/state-persistor/state-persistor-bgstyle.service.ts
+++ src/services/state-persistor/state-persistor-bgstyle.service.ts
@@ -5,8 +5,6 @@ import { storageStyleMapper } from './state-persistor-bgstyle.mapper'
 import { storageHelper } from './storage/storage.helper'
 import { useStyleStore } from '@/stores/style.store'
 import { useMapStore } from '@/stores/map.store'
-import { SP_KEY_SERIAL } from './state-persistor.model'
-import { StyleItem } from '@/composables/mvt-styles/mvt-styles.model'
 
 class StatePersistorStyleService {
   styleWatcher: WatchStopHandle
@@ -17,7 +15,7 @@ class StatePersistorStyleService {
     // eslint-disable-next-line prefer-const
     stop = watchEffect(() => {
       if (styleStore.bgVectorSources && mapStore.bgLayer) {
-        this.restore(false)
+        this.restore()
         this.persist()
         stop && stop() // test if exists, for HMR support
       }
@@ -45,14 +43,8 @@ class StatePersistorStyleService {
                   value || [],
                   storageStyleMapper.styleToLocalStorage
                 )
-                storageHelper.setValue(
-                  SP_KEY_SERIAL,
-                  value || [],
-                  storageStyleMapper.styleToSerial
-                )
               } else {
                 storageHelper.removeItem(mapStore.bgLayer.name)
-                storageHelper.removeItem(SP_KEY_SERIAL)
               }
             }
           }
@@ -62,25 +54,16 @@ class StatePersistorStyleService {
     }
   }
 
-  restore(localStorageOnly: boolean) {
+  restore() {
     const styleStore = useStyleStore()
     styleStore.setStyle(null)
     const mapStore = useMapStore()
     const bgLayer = mapStore.bgLayer
     if (bgLayer) {
-      let bgStyle: StyleItem[] = []
-      if (!localStorageOnly) {
-        bgStyle = storageHelper.getValue(
-          SP_KEY_SERIAL,
-          storageStyleMapper.styleSerialToStyle
-        )
-      }
-      if (bgStyle.length === 0) {
-        bgStyle = storageHelper.getValue(
-          bgLayer.name,
-          storageStyleMapper.styleLocalStorageToStyle
-        )
-      }
+      const bgStyle = storageHelper.getValue(
+        bgLayer.name,
+        storageStyleMapper.styleLocalStorageToStyle
+      )
       if (bgStyle && bgStyle.length > 0) {
         styleStore.setStyle(bgStyle)
       }
