657892f66c937e679aed7093ebf1b01be4fccdad
geoportallux <5592871+geoportallux@users.noreply.github.com>
2026-04-24 09:57:18 +0200

    Update adresse-template.vue
    and one more

--- src/components/info/templates/adresse-template.vue
+++ src/components/info/templates/adresse-template.vue
@@ -3,21 +3,19 @@ import { FeatureInfoJSON } from '@/services/info/feature-info.model'
 import InfoFeatureLayout from '../info-feature-layout.vue'
 import { computed } from 'vue'
 
-const props = defineProps<{
+defineProps<{
   layers: FeatureInfoJSON
   currentUrl?: string
 }>()
 
-const formattedLabel = computed(() => (label: string) => {
-  return label.replace(/\n/g, '<br>')
-})
+const formatLabel = (label: string) => label.replace(/\n/g, '<br>')
 </script>
 
 <template>
   <InfoFeatureLayout :layers="layers" :currentUrl="currentUrl">
     <template #feature-content="{ feature }">
       <h4>{{ feature.attributes.num_affaire }}</h4>
-      <div v-html="formattedLabel(feature.attributes.label)"></div>
+      <div v-html="formatLabel(feature.attributes.label)"></div>
     </template>
   </InfoFeatureLayout>
 </template>
