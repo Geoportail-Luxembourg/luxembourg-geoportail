997693b4ef0dfc7536bdfdae1bdd076555307c18
geoportallux <5592871+geoportallux@users.noreply.github.com>
2026-04-24 09:54:30 +0200

    Update adresse-template.vue
    next try after linting error

--- src/components/info/templates/adresse-template.vue
+++ src/components/info/templates/adresse-template.vue
@@ -1,26 +1,23 @@
 <script setup lang="ts">
 import { FeatureInfoJSON } from '@/services/info/feature-info.model'
 import InfoFeatureLayout from '../info-feature-layout.vue'
-defineProps<{
+import { computed } from 'vue'
+
+const props = defineProps<{
   layers: FeatureInfoJSON
   currentUrl?: string
 }>()
+
+const formattedLabel = computed(() => (label: string) => {
+  return label.replace(/\n/g, '<br>')
+})
 </script>
+
 <template>
   <InfoFeatureLayout :layers="layers" :currentUrl="currentUrl">
     <template #feature-content="{ feature }">
       <h4>{{ feature.attributes.num_affaire }}</h4>
       <div v-html="formattedLabel(feature.attributes.label)"></div>
     </template>
-    
-    <script>
-    export default {
-      computed: {
-        formattedLabel() {
-          return (label) => label.replace(/\n/g, '<br>');
-        }
-      }
-    }
-    </script>
   </InfoFeatureLayout>
 </template>
