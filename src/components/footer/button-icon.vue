f234760e0a96f90fdcaefc1ddfb41135d54d2f24
Michaëlis <renaud.michaelis@act.etat.lu>
2026-06-12 15:51:11 +0200

    new icon from font-awsome

--- src/components/footer/button-icon.vue
+++ src/components/footer/button-icon.vue
@@ -4,6 +4,7 @@ const props = defineProps<{
   icon: string
   active?: boolean
   ariaLabel?: string
+  iconClass?: string
 }>()
 </script>
 
@@ -15,12 +16,21 @@ const props = defineProps<{
     :aria-pressed="props.active !== undefined ? props.active : undefined"
   >
     <span
+      v-if="props.active || !props.iconClass"
       class="block text-[1.7rem] sm:text-[2rem] -mt-1.5 -mb-3 after:font-icons after:w-8 after:max-h-4 after:inline-block"
       :class="props.active ? 'lux-close-cross' : `after:content-${props.icon}`"
       aria-hidden="true"
     ></span>
-    <span class="block text-[13px] sm:text-base uppercase">{{
-      props.label
-    }}</span>
+    <span
+      v-else
+      class="block text-[1.7rem] sm:text-[2rem] -mt-1.5 -mb-3"
+      :class="props.iconClass"
+      aria-hidden="true"
+    ></span>
+    <span
+      class="block text-[13px] sm:text-base uppercase"
+      :class="props.iconClass && !props.active ? 'translate-y-px' : ''"
+      >{{ props.label }}</span
+    >
   </button>
 </template>
