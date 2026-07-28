2aa2c05a528824b214eec608cfbecc167251c7ca
Renaud Michaëlis <renaud.michaelis@gmail.com>
2026-07-28 14:54:20 +0200

    Display attributions for all the layers, not only for the bg (#350)

--- src/components/map-controls/attribution-control.vue
+++ src/components/map-controls/attribution-control.vue
@@ -1,12 +1,28 @@
 <script setup lang="ts">
 import { useMapStore } from '@/stores/map.store'
-import { watchEffect, ref } from 'vue'
+import { computed } from 'vue'
 
 const mapStore = useMapStore()
-const attribution = ref('')
 
-watchEffect(() => {
-  attribution.value = mapStore.bgLayer?.metadata?.attribution || ''
+const attributions = computed(() => {
+  const allAttributions: string[] = []
+
+  // Ajouter l'attribution de la couche d'arrière-plan si elle existe
+  if (mapStore.bgLayer?.metadata?.attribution) {
+    allAttributions.push(mapStore.bgLayer.metadata.attribution)
+  }
+
+  // Ajouter les attributions des couches superposées actives
+  mapStore.layers.forEach(layer => {
+    if (layer.metadata?.attribution) {
+      allAttributions.push(layer.metadata.attribution)
+    }
+  })
+  return allAttributions
+})
+
+const combinedAttribution = computed(() => {
+  return attributions.value.join(' | ')
 })
 </script>
 
@@ -14,7 +30,7 @@ watchEffect(() => {
   <div
     data-cy="attributionControl"
     class="absolute bottom-0 z-10 text-[0.8em] px-[4px] text-[#6b818f] bg-[#ffffffb3]"
-    v-if="attribution"
-    v-dompurify-html="attribution"
+    v-if="combinedAttribution"
+    v-dompurify-html="combinedAttribution"
   ></div>
 </template>
