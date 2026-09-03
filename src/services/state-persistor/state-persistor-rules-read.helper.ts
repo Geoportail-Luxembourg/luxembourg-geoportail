e1dff774cd4823dd753a4364d8a844fca055df27
zurfjereluhmie <jeremie.zurfluh@camptocamp.com>
2026-09-03 11:17:55 +0200

    GSLUX-815: expanded nodes only in localstorage

--- src/services/state-persistor/state-persistor-rules-read.helper.ts
+++ src/services/state-persistor/state-persistor-rules-read.helper.ts
@@ -2,6 +2,7 @@ import {
   ParamKeys,
   SP_AVAILABLE_STORAGES,
   SP_KEY_APPLOGIN,
+  SP_KEY_EXPANDED_NODES,
   SP_KEY_IPV6,
   SP_KEY_LOCALFORAGE,
   SP_KEY_THEME,
@@ -40,6 +41,9 @@ export abstract class RulesReadHelper extends StatePersistorRulesHelper {
       return SP_AVAILABLE_STORAGES.permalink
     }
     if (key === SP_KEY_LAYERS_OPEN) {
+      return SP_AVAILABLE_STORAGES.localStorage
+    }
+    if (key === SP_KEY_EXPANDED_NODES) {
       return SP_AVAILABLE_STORAGES.localStorage
     }
     return undefined
